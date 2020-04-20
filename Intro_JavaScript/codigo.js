4dfcefe74e08c982b991a3369bf191763cb689e4
randymxd06 <randym0624@gmail.com>
2020-04-20 14:43:34 -0400

    actualiza todo

---
+++ Intro_JavaScript/codigo.js
@@ -0,0 +1,19 @@
+        const inputColor = document.querySelector("#color");
+        const btnCambiarColor = document.querySelector("#cambiar_color");
+
+        btnCambiarColor.addEventListener('click', () =>{
+
+            let color = inputColor.value;
+
+            if(color == ''){
+                alert("Error, campo de texto vacío");
+            }
+            else{
+               CambiarColorFondo(color); 
+            }
+
+        });
+
+        function CambiarColorFondo(color){
+            document.body.style.background = color;
+        }
